3501185d51d240548048bb2af0954a32c87c6ee8
chrisw099 <chris.woolston9@gmail.com>
2023-11-30 03:45:18 -0500

    merge'

--- Listener/ListenerServer.js
+++ Listener/ListenerServer.js
@@ -6,6 +6,29 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname +'/static'));
 app.use(express.json())
 
+app.use(express.urlencoded({extended: true}));
+var session = require('express-session');
+var parseurl = require('parseurl');
+
+app.set('trust proxy', 1)
+app.use(session({
+    secret : 'View Counter',
+    resave : false,
+    saveUninitialized: true,
+}))
+
+app.use(function(req, res, next){
+    if(!req.session.views){
+        req.session.views={}
+    }
+    
+    var pathname = parseurl(req).pathname
+    
+    req.session.views[pathname] = (req.session.views[pathname] || 0) + 1
+
+    next()
+})
+
 mongoose.connect('mongodb://127.0.0.1:27017/swe432');
 const db = mongoose.connection;
 
@@ -27,6 +50,14 @@ const listenerPreferences = new mongoose.Schema({
   LikedGenres: String,
   DislikedGenres: String
 });
+
+//timeslot for producer
+const timeslotSchema = new mongoose.Schema({
+  timeslot: String,
+  dj: String,
+  playlist: String,
+  songs: [songSchema]
+  });
 
 db.once('open', () => {
     console.log('DEBUG: Mongo session has been connected');
@@ -93,6 +124,146 @@ app.post('/ListenerUpdate*', async function(req, res){
   res.json(userdata);
   console.log("Server Updated, "+userdata)
 })
+
+//Producer part
+const constTimeslots = [
+  { time: '9:00 AM - 10:00 AM' },
+  { time: '10:00 AM - 11:00 AM' },
+  { time: '11:00 AM - 12:00 PM' },
+  { time: '12:00 PM - 1:00 PM' },
+  { time: '1:00 PM - 2:00 PM' },
+  { time: '2:00 PM - 3:00 PM' },
+  { time: '3:00 PM - 4:00 PM' },
+  { time: '4:00 PM - 5:00 PM' },
+  { time: '5:00 PM - 6:00 PM' },
+  { time: '6:00 PM - 7:00 PM' },
+  { time: '7:00 PM - 8:00 PM' },
+  { time: '8:00 PM - 9:00 PM' },
+  { time: '9:00 PM - 10:00 PM' },
+];
+
+const Songs = mongoose.model('SongList', songSchema, 'SongList');
+const PlaylistP = mongoose.model('PlaylistP', timeslotSchema, 'Playlist');
+
+// producer page
+app.get('/Producer', async function(req, res) {
+  let songslist = await Songs.find();
+  let playlistDB = await PlaylistP.find();
+
+  res.render('pages/Producer', { 
+    DBsongs: songslist, 
+    timeslots: constTimeslots,
+    playlist : playlistDB
+   });
+});
+
+// producer add songs
+app.post('/addSong', async (req, res) => {
+  const selectedSongTitle = req.body.selectedSong;
+  const formTimeslot = req.body.selectedTimeslot;
+
+  const selectedSong = await Songs.findOne({title: selectedSongTitle});
+  if (!selectedSong) {
+    return res.status(404).json({ error: 'Selected song not found' });
+  }
+
+  const playlist = await PlaylistP.findOne({timeslot: formTimeslot});
+  if (!playlist) {
+    return res.status(404).json({error: 'Playlist not found'});
+  }
+
+  playlist.songs.push(selectedSong);
+  await playlist.save();
+
+  res.redirect('/');
+});
+
+const Playlist = require('./models/Playlist.js')
+const Song = require('./models/Song.js')
+const Timeslot = require('./models/Timeslot.js')
+
+app.get('/dj', async function(req, res){
+
+  const plOpt = await Playlist.find({}, {Name: 1, Songs:1});
+  const sOpt = await Song.find({}, {Title: 1});
+  const tOpt = await Timeslot.find({}, {Timeslot:1, Playlist:1, Dj:1 })
+
+  console.log("plopt:" + plOpt);
+
+ console.log('you viewed this page ' + req.session.views['/'] + ' times')
+
+  res.render('pages/djPage', {
+      plOptions: plOpt,
+      sOption : sOpt,
+      tOption : tOpt
+
+  });
+
+
+});
+
+app.post('/plUpdate', async function(req, res){
+
+  const timeSlot = req.body.tSlots
+  const song = req.body.songs
+
+  const cPList = await Timeslot.findOne({Timeslot: timeSlot}, {Playlist:1})
+
+  console.log("Playlist: " + cPList);
+
+  var newSong = await Song.findOne({"Title" : song},{});//{name: song};
+
+  console.log("New Song :" + newSong);
+
+  var updateLog = await db.collection("playlists").findOneAndUpdate({'Name' : cPList.Playlist}, {$push : {Songs: newSong}});
+
+  console.log ("Song is "+ song);
+  console.log("Timeslot is " + timeSlot);
+  console.log("Playlist is "+ cPList);
+  console.log("UpdateLog : " + updateLog)
+
+  const plOpt = await Playlist.find({}, {Name: 1, Songs:1});
+  const sOpt = await Song.find({}, {Title: 1});
+  const tOpt = await Timeslot.find({}, {Timeslot:1, Playlist:1, Dj:1 })
+
+  res.render('pages/djPage', {
+      plOptions: plOpt,
+      sOption : sOpt,
+      tOption : tOpt
+
+  });
+});
+
+app.get('/filterSongs', async function(req,res){
+
+  const Playlist = require('./models/Playlist.js')
+  let search =  req.query.search;
+
+  //console.log("Search : " + search);
+
+  let songList = await Playlist.findOne({'Name' : search},{Songs:1});
+
+  //console.log("result: " + songList);
+
+  res.json(songList);
+});
+
+app.get('/tsLookup', async function(req,res){
+
+  const Playlist = require('./models/Timeslot.js')
+  let search =  req.query.search;
+
+  //console.log("Search : " + search);
+
+  let tsList = await Timeslot.findOne({'Timeslot' : search},{Timeslot: 1, Dj: 1, Playlist:1});
+
+  //console.log(tsList)
+
+  res.json(tsList);
+});
+
+
+
 
 console.log("DEBUG: Server listening in 8080")
 app.listen(8080)
